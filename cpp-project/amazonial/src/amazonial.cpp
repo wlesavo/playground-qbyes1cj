0c76edce86cfe9f1a2482769342916f5101f3acf
Marat Gilmanov <m.gilmanov@smartengines.ru>
2022-04-07 16:47:42 +0300

    add: amazonial

--- cpp-project/amazonial/src/amazonial.cpp
+++ cpp-project/amazonial/src/amazonial.cpp
@@ -7,20 +7,21 @@
 #endif
 #include <iostream>
 #include <chrono>
-//}
-
-#include <immintrin.h>
-#include <bits/stdc++.h>
 using namespace std;
 using namespace std::chrono;
-#define uint128_t __int128
-
-//{
     
-const int WIDTH = 9;
-const int HEIGHT = 9;
-const int SIZE = 9 * 9;
+const int WIDTH = 8;
+const int HEIGHT = 8;
+const int SIZE = 8 * 8;
 
+template <class T>
+static int count_set_bits(T n)
+{
+  if (n == 0)
+    return 0;
+  else
+    return (n & 1) + countSetBits(n >> 1);
+}
 template <class T>
 static inline void set_bit(T& val, int n){
   val |= (T)1 << n;
@@ -36,24 +37,10 @@ static inline bool check_bit(const T& a, int n){
 
 class State {
 public:  
-  uint128_t con[4] = {};
-  int player_pos[3] = {};
+  uint64_t con[8] = {};
+  int player_pos[2][2] = {};
+  int voronoi_score[2] = {};
   State() {};
-  void set_wall(int pos, int type){
-    if (type == 0){
-      reset_bit(con[2], pos);
-      reset_bit(con[2], pos+1);
-      reset_bit(con[0], pos+WIDTH);
-      reset_bit(con[0], pos+WIDTH+1);
-    }
-    else{
-      reset_bit(con[1], pos);
-      reset_bit(con[1], pos+WIDTH);
-      reset_bit(con[3], pos+1);
-      reset_bit(con[3], pos+1+WIDTH);
-    }
-  }
-
 };
 //}
 
@@ -61,61 +48,71 @@ class Game {
 public:
 
   State cur_state{};
-  uint128_t player_won[3] = {};
-  int bfs_rolls = 0;
+  int voronoi_rolls = 0;
   Game() {
     // activate all connections
     for (int i = 0; i < 4; i++){
-      cur_state.con[i] = ((uint128_t)1 << 81) - (uint128_t)1;
+      cur_state.con[i] = ((uint64_t)1 << 64) - (uint64_t)1;
     }
     // remove boundary connections
     for (int i = 0; i < WIDTH; i++) {
       reset_bit(cur_state.con[0], i);
+      reset_bit(cur_state.con[1], i);
       reset_bit(cur_state.con[1], i * WIDTH + WIDTH - 1);
-      reset_bit(cur_state.con[2], i + WIDTH * (HEIGHT - 1));
-      reset_bit(cur_state.con[3], i * WIDTH);
-    }
-    
-    // prepare win-condition masks
-    for (int i = 0; i < WIDTH; i++){     
-      set_bit(player_won[0],(i*WIDTH + HEIGHT - 1));
-      set_bit(player_won[1], i*WIDTH);
-      set_bit(player_won[2], (HEIGHT - 1)*HEIGHT + i);      
-    }
-    
+      reset_bit(cur_state.con[2], i * WIDTH + WIDTH - 1);
+      reset_bit(cur_state.con[3], i * WIDTH + WIDTH - 1);
+      reset_bit(cur_state.con[3], i + WIDTH * (HEIGHT - 1));
+      reset_bit(cur_state.con[4], i + WIDTH * (HEIGHT - 1));      
+      reset_bit(cur_state.con[5], i + WIDTH * (HEIGHT - 1));
+      reset_bit(cur_state.con[5], i * WIDTH);
+      reset_bit(cur_state.con[6], i * WIDTH);
+      reset_bit(cur_state.con[7], i * WIDTH);
+      reset_bit(cur_state.con[7], i);
+    }    
   }
   
-  int get_path_len(State& s, int owner){
-    int pos = s.player_pos[owner];
-    if (check_bit(player_won[owner], pos)){
-      return 0;
+  void voronoi(State& s){
+  // set strating positions for both units
+    for (int i = 0; i<2; i++){
+      int pos1 = s.player_pos[0][i];
+      set_bit(cur1, pos1);
+      int pos2 = s.player_pos[1][i];
+      set_bit(cur2, pos2);
     }
-    bfs_rolls += 1;
-    uint128_t win = player_won[owner];
-    uint128_t cur = (uint128_t)1<<pos;
-    uint128_t visits = cur;
-    int dist = 0;
-    while (true) {
-      if (cur==0){
-        return -1;
-      }
-      if ((cur&win)>0){
-        return dist;
-      }
-      cur = (cur&s.con[0])>>WIDTH | (cur&s.con[1])<<1 | (cur&s.con[2])<<WIDTH | (cur&s.con[3])>>1;
-      cur &= ~visits;
-      visits |= cur;
-      dist += 1;
+    uint64_t my_cells = cur1;
+    uint64_t opp_cells = cur2;
+    uint64_t visited = cur1 | cur2;
+    voronoi_rolls += 1;
+    
+    while (cur1!=0 || cur2 != 0) {
+      cur1 = (cur1 & s.con[0]) >> w | (cur1 & s.con[1]) >> (w - 1) 
+           | (cur1 & s.con[2]) << 1 | (cur1 & s.con[3]) << (w + 1) 
+           | (cur1 & s.con[4]) << w | (cur1 & s.con[5]) << (w - 1) 
+           | (cur1 & s.con[6]) >> 1 | (cur1 & s.con[7]) >> (w + 1);
+      cur1 &= ~visited;
+    
+      cur2 = (cur2 & s.con[0]) >> w | (cur2 & s.con[1]) >> (w - 1) 
+           | (cur2 & s.con[2]) << 1 | (cur2 & s.con[3]) << (w + 1) 
+           | (cur2 & s.con[4]) << w | (cur2 & s.con[5]) << (w - 1) 
+           | (cur2 & s.con[6]) >> 1 | (cur2 & s.con[7]) >> (w + 1);
+      cur2 &= ~visited;
+      
+      my_cells  |= (cur1 & (~cur2));
+      opp_cells |= (cur2 & (~cur1));
+    
+      visited |= cur1 | cur2;
     }
-    return -1;    
-  }
+    s.voronoi_score[0] = count_set_bits(my_cells)
+    s.voronoi_score[1] = count_set_bits(opp_cells);
+}
 
   void init_cur_state(){
     //init player position
-    cur_state.player_pos[0] = 2 + 4 * WIDTH;
-    //add some vertical walls
-    cur_state.set_wall(3+3*WIDTH, 1);
-    cur_state.set_wall(4+4*WIDTH, 1);
+    cur_state.player_pos[0][0] = 2 + 4 * WIDTH;
+    cur_state.player_pos[0][1] = 3 + 5 * WIDTH;
+    cur_state.player_pos[1][0] = 1 + 2 * WIDTH;
+    cur_state.player_pos[1][1] = 4 + 6 * WIDTH;
+    
   }
   
   void test() {
@@ -132,12 +129,12 @@ public:
     int roll_count = 1000000;
     auto start = high_resolution_clock::now();
     for (int i = 0; i<roll_count; i++){
-        total_dist += get_path_len(cur_state, 0);
+        voronoi(cur_state, 0);
     }
     auto end = high_resolution_clock::now();
     auto full_count = duration_cast<microseconds>(end - start).count()/1000;
-    cerr << "total execution time " << full_count << "ms with " << bfs_rolls << " rolls"<<  endl;
-    cerr << "calculated min distance " << total_dist/bfs_rolls << endl;
+    cerr << "total execution time " << full_count << "ms with " << voronoi_rolls << " rolls"<<  endl;
+    cerr << "calculated voronoi score player 1: " << cur_state.voronoi_score[0] << " player 2: " << cur_state.voronoi_score[1] << endl;
   }
 };
 
--- markdowns/amazonial.md
+++ markdowns/amazonial.md
@@ -1,4 +1,60 @@
 # Amazonial
+As was previously said, we will now try to apply similar concept to solve a quite different task. Namely we will calculate the number of cells one player can reach faster than another and vise versa, which is a special case of [Voronoi diagrams] (https://www.codingame.com/playgrounds/243/voronoi-diagrams/fortunes-algorithm) calculated on a grid.
+
+# General idea
+The game Amazonial is not that different from great escape if you look on it at the right angle. Although it still requires quite a few modifications, i.e accounting for multiple units and 8 available dirrections. The later will lead to a modification of our BFS as
+``` cpp
+cur = (cur & con[0]) >> w | (cur & con[1]) >> (w - 1) 
+     | (cur & con[2]) << 1 | (cur & con[3]) << (w + 1) 
+     | (cur & con[4]) << w | (cur & con[5]) << (w - 1) 
+     | (cur & con[6]) >> 1 | (cur & con[7]) >> (w + 1);
+```
+
+The rest is simple. We first calculate new positions on a current depth for both players, by expanding current cells and substracting all the cells already visited by any player. Then the cells that only one player reached on a current depth are added to the positions that has been won. Then we finally update visited cells. After the BFSs for both players are finished, we calculate the area by counting set bits with a simple recursive function.
+
+``` cpp
+template <class T>
+static int count_set_bits(T n)
+{
+  if (n == 0)
+    return 0;
+  else
+    return (n & 1) + countSetBits(n >> 1);
+}
+
+void voronoi(const State& s){
+  // set strating positions for both units
+  for (int i = 0; i<2; i++){
+    int pos1 = s.players[0].pos[i];
+    set_bit(cur1, pos1);
+    int pos2 = s.players[1].pos[i];
+    set_bit(cur2, pos2);
+  }
+  uint64_t my_cells = cur1;
+  uint64_t opp_cells = cur2;
+  uint64_t visited = cur1 | cur2;
+  while (cur1!=0 || cur2 != 0) {
+    cur1 = (cur1 & s.con[0]) >> w | (cur1 & s.con[1]) >> (w - 1) 
+         | (cur1 & s.con[2]) << 1 | (cur1 & s.con[3]) << (w + 1) 
+         | (cur1 & s.con[4]) << w | (cur1 & s.con[5]) << (w - 1) 
+         | (cur1 & s.con[6]) >> 1 | (cur1 & s.con[7]) >> (w + 1);
+    cur1 &= ~visited;
+  
+    cur2 = (cur2 & s.con[0]) >> w | (cur2 & s.con[1]) >> (w - 1) 
+         | (cur2 & s.con[2]) << 1 | (cur2 & s.con[3]) << (w + 1) 
+         | (cur2 & s.con[4]) << w | (cur2 & s.con[5]) << (w - 1) 
+         | (cur2 & s.con[6]) >> 1 | (cur2 & s.con[7]) >> (w + 1);
+    cur2 &= ~visited;
+    
+    my_cells  |= (cur1 & (~cur2));
+    opp_cells |= (cur2 & (~cur1));
+  
+    visited |= cur1 | cur2;
+  }
+  int my_score = count_set_bits(my_cells)
+  int opp_score = count_set_bits(opp_cells);
+}
+```
 
 # Code example
 
